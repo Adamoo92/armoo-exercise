d963e59922800fad0bcd2dbb5b51a380885c41ed
Adamoo92 <daichangle1203@gmail.com>
2022-10-06 14:49:43 +0800

    1006-1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import Login from "./components/Login";
 import "./styles.scss";
+import data from "./data/color.json";
+
+const Color = (color) => {
+  data["framer-login-page"]["Primary"][color].value;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  return <div className="App">{Color("Purple")}</div>;
 }
+console.log(data["framer-login-page"]["Primary"]["Purple"].value);
 
 export default App;
