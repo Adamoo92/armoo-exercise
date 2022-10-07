1a389f28ce893a7330fa343885db4580488ac67c
Adamoo92 <daichangle1203@gmail.com>
2022-10-07 11:12:11 +0800

    login page complete

--- src/components/LoginEmail.jsx
+++ src/components/LoginEmail.jsx
@@ -4,19 +4,23 @@ import {
   IconCheck,
   IconCircleMultiply,
   IconEye,
+  IconEyeSlash,
   IconGoogle,
   IconLogo,
   IconWeibo,
 } from "./Icon";
-import { motion } from "framer-motion";
+import { motion, useTime, useTransform } from "framer-motion";
 import { Color } from "./Color";
+import { IllustratorLogin } from "./Illustrator";
 
 const tabs = ["手机登录", "邮箱登录"];
 
 const ePattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
 const LoginEmail = () => {
+  const [divScaleX, setDivScaleX] = useState(0);
   const [selectedTab, setSelectedTab] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
   const [inputValue, setInputValue] = useState({
     email: "",
     password: "",
@@ -41,111 +45,156 @@ const LoginEmail = () => {
 
   const handleVerify = () => {
     if (inputValue.email === "") {
-      setMessage({ email: "邮箱地址不能为空" });
-      setError({ email: true });
+      setMessage((prev) => ({ ...prev, email: "邮箱地址不能为空" }));
+      setError((prev) => ({ ...prev, email: true }));
     } else if (!ePattern.test(inputValue.email)) {
-      setMessage({ email: "邮箱格式不正确" });
-      setError({ email: true });
+      setMessage((prev) => ({ ...prev, email: "邮箱格式不正确" }));
+      setError((prev) => ({ ...prev, email: true }));
     } else {
-      setMessage(null);
-      setError({ email: false });
+      setMessage((prev) => ({ ...prev, email: "" }));
+      setError((prev) => ({ ...prev, email: false }));
     }
     if (inputValue.password === "") {
-      setMessage({ password: "密码不能为空" });
+      setMessage((prev) => ({ ...prev, password: "密码不能为空" }));
+      setError((prev) => ({ ...prev, password: true }));
     }
+  };
+
+  const handleVerifyPassword = () => {
+    if (inputValue.password === "") {
+      setMessage((prev) => ({ ...prev, password: "密码不能为空" }));
+      setError((prev) => ({ ...prev, password: true }));
+    }
+    setMessage((prev) => ({ ...prev, password: "" }));
+    setError((prev) => ({ ...prev, password: false }));
   };
 
   console.log(inputValue);
 
   return (
-    <div className="logincontainer">
-      <IconLogo />
-      <ul className="tabs">
-        {tabs.map((item, i) => (
-          <li
-            key={i}
-            className={i === selectedTab ? "selected" : ""}
-            onClick={() => setSelectedTab(i)}
-          >
-            {item}
-            {i === selectedTab ? (
-              <motion.div className="underline" layoutId="underline" />
-            ) : null}
-          </li>
-        ))}
-      </ul>
-      <form>
-        <div className="textfield-container">
-          <div
-            className="input-container"
-            style={{
-              color: error.email ? Color.Red : Color.Gray01,
-            }}
-          >
-            <input
-              placeholder="请输入邮箱"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleVerify}
+    <>
+      <div className="logincontainer">
+        <div className="progress">
+          <motion.div
+            style={{ originX: 0 }}
+            animate={{ scaleX: divScaleX }}
+            transition={{ ease: "easeInOut", duration: 1 }}
+          />
+        </div>
+        <IconLogo />
+        <ul className="tabs">
+          {tabs.map((item, i) => (
+            <li
+              key={i}
+              className={i === selectedTab ? "selected" : ""}
+              onClick={() => setSelectedTab(i)}
+            >
+              {item}
+              {i === selectedTab ? (
+                <motion.div className="underline" layoutId="underline" />
+              ) : null}
+            </li>
+          ))}
+        </ul>
+        <form>
+          <div className="textfield-container">
+            <div
+              className="input-container"
               style={{
-                borderColor: error.email ? Color.Red : "",
-                backgroundColor: error.email ? "rgba(250, 77, 86, 0.05)" : "",
+                color: error.email ? Color.Red : Color.Gray01,
               }}
-            />
-            {error.email ? <IconCircleMultiply /> : null}
+            >
+              <input
+                placeholder="请输入邮箱"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleVerify}
+                style={{
+                  borderColor: error.email ? Color.Red : "",
+                  backgroundColor: error.email ? "rgba(250, 77, 86, 0.05)" : "",
+                }}
+              />
+              {error.email ? <IconCircleMultiply /> : null}
+            </div>
+            <span
+              className="message"
+              style={{ color: error.email ? Color.Red : "" }}
+            >
+              {message.email}
+            </span>
           </div>
-          <span
-            className="message"
-            style={{ color: error.email ? Color.Red : "" }}
-          >
-            {message.email}
-          </span>
-        </div>
-        <div className="textfield-container">
-          <div className="input-container">
-            <input
-              placeholder="请输入密码"
-              name="password"
-              onBlur={handleVerify}
-              onChange={handleChange}
-            />
-            <IconEye />
+          <div className="textfield-container">
+            <div
+              className="input-container"
+              style={{
+                color: error.email ? Color.Red : Color.Gray01,
+              }}
+            >
+              <input
+                placeholder="请输入密码"
+                name="password"
+                onBlur={handleVerifyPassword}
+                onChange={handleChange}
+                type={isVisible ? "text" : "password"}
+                style={{
+                  borderColor: error.password ? Color.Red : "",
+                  backgroundColor: error.password
+                    ? "rgba(250, 77, 86, 0.05)"
+                    : "",
+                }}
+              />
+              <i
+                onClick={() => setIsVisible(isVisible ? false : true)}
+                style={{ color: error.password ? Color.Red : "" }}
+              >
+                {isVisible ? <IconEye /> : <IconEyeSlash />}
+              </i>
+            </div>
+            <span
+              className="message"
+              style={{ color: error.password ? Color.Red : "" }}
+            >
+              {message.password}
+            </span>
+            <span isbutton="true">忘记密码？</span>
           </div>
-          <span
-            className="message"
-            style={{ color: error.email ? Color.Red : "" }}
+          <button
+            type="button"
+            canClick={
+              !inputValue.email ||
+              !inputValue.password ||
+              error.email ||
+              error.password
+                ? "false"
+                : "true"
+            }
+            onClick={() => setDivScaleX(1)}
           >
-            {message.password}
-          </span>
-          <span isbutton="true">忘记密码？</span>
+            登 录
+          </button>
+        </form>
+        <div className="signup">
+          没有账户？ <span>创建一个</span>
         </div>
-        <button
-          type="button"
-          disabled={error.email && error.password ? false : true}
-        >
-          登 录
-        </button>
-      </form>
-      <div className="signup">
-        没有账户？ <span>创建一个</span>
+        <div className="divider">
+          <i></i>
+          <span>其他登录方式</span>
+          <i></i>
+        </div>
+        <div className="othersignin">
+          <i>
+            <IconGoogle />
+          </i>
+          <i>
+            <IconApple />
+          </i>
+          <i>
+            <IconWeibo />
+          </i>
+        </div>
       </div>
-      <div className="divider">
-        <i></i>
-        <span>其他登录方式</span>
-        <i></i>
-      </div>
-      <div className="othersignin">
-        <i>
-          <IconGoogle />
-        </i>
-        <i>
-          <IconApple />
-        </i>
-        <i>
-          <IconWeibo />
-        </i>
-      </div>
-    </div>
+      <IllustratorLogin />
+    </>
   );
 };
 
