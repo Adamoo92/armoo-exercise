bde38132cedffe8d9afe2659801b4c543f798f42
Adamoo92 <daichangle1203@gmail.com>
2022-10-08 17:15:13 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+const markdown = `
+Here is some JavaScript code:
+
+~~~js
+console.log('It works!')
+~~~
+`;
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <ReactMarkdown
+        children={markdown}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, "")}
+                style={dark}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/assets/github.md
@@ -0,0 +1,20 @@
+cd 进
+git init 初始化
+git add .
+git commit -m '"first-commit"
+
+SSH链接
+git remote add origin git@github.com:Adamoo92/ ... .git
+git push -u origin main
+
+### 更改当前分支
+git branch -m main
+
+### 查看分支
+git branch
+
+### 切换分支
+git checkout XXX
+
+### 删除分支
+git branch -d XXX
